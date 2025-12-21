685585c079aa99b14dc247be2eb5b5a0845f6d6e
RezzyQ <nlreyf202@gmail.com>
2025-12-21 11:15:01 +0300

    Update crypto.cpp

--- client/crypto.cpp
+++ client/crypto.cpp
@@ -1,17 +1,35 @@
+/**
+ * @file crypto.cpp
+ * @author Кошкин М.В.
+ * @version 1.0
+ * @date 2025
+ * @copyright ИБСТ ПГУ
+ * @brief Реализация криптографических функций
+ * @warning Используется алгоритм MD5, который считается криптографически нестойким
+ */
+
 #include "crypto.h"
 
+/**
+ * @brief Функция аутентификации с использованием MD5 хеширования
+ * @param[in] salt Соль для хеширования
+ * @param[in] pass Пароль пользователя
+ * @return Хеш-строка в hex-формате
+ * @warning Использует алгоритм MD5, который считается криптографически нестойким
+ * @details Вычисляет MD5 хеш от конкатенации соли и пароля, результат возвращает в hex-формате
+ */
 string auth(string salt, string pass){
-
-CPP::Weak1::MD5 md5;
-string hash;
-CPP::StringSource(
-    string(salt).append(pass), 
-    true,
+    CPP::Weak1::MD5 md5; ///< Объект для вычисления MD5 хеша
+    string hash; ///< Результирующий хеш
+    
+    CPP::StringSource(
+        string(salt).append(pass), ///< Конкатенация соли и пароля
+        true,
         new CPP::HashFilter(
-            md5,
-            new CPP::HexEncoder(
+            md5, ///< Использование MD5 для хеширования
+            new CPP::HexEncoder( ///< Кодирование результата в hex
                 new CPP::StringSink(hash)))
-);
+    );
 
-return hash;
+    return hash; ///< Возврат хеша в hex-формате
 }
