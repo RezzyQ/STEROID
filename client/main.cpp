f25ea3d95430aa642ceff1475d9c707da482153a
RezzyQ <nlreyf202@gmail.com>
2025-12-21 11:19:08 +0300

    Update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -1,18 +1,29 @@
-#include <iostream>
-#include "connection.cpp"
+#include "connection.h"
 #include "interface.h"
-#include "errno.h"
-#include <system_error>
-using namespace std;
 
+/**
+ * @brief Главная функция приложения
+ * @param[in] argc Количество аргументов командной строки
+ * @param[in] argv Массив аргументов командной строки
+ * @return 0 при успешном выполнении, 1 при ошибке параметров
+ * @details Функция выполняет:
+ * - Парсинг параметров командной строки
+ * - Проверку корректности параметров
+ * - Установку соединения с сервером и обработку данных
+ */
 int main(int argc, const char** argv)
 {
-    Interface interface;
+    UserInterface interface; ///< Объект для работы с пользовательским интерфейсом
+    
+    // Парсинг аргументов командной строки
     if(!interface.Parser(argc, argv)){
         cout << interface.getDescription() << endl;
         return 1;
     }
+    
+    // Получение параметров и установка соединения
     Params params = interface.getParams();
-    conn(&params);
+    Connection::conn(&params);
+    
     return 0;
 }
