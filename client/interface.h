69e79a9ca33349e4b46982c698ec5b1b469cb7d5
RezzyQ <nlreyf202@gmail.com>
2025-12-21 11:18:51 +0300

    Update interface.h

--- client/interface.h
+++ client/interface.h
@@ -4,29 +4,57 @@
 #include <sstream>
 using namespace std;
 namespace po = boost::program_options;
-// структура для хранения полученных параметров
+
+/**
+ * @struct Params
+ * @brief Структура для хранения параметров командной строки
+ * @details Содержит все необходимые параметры для установки соединения и работы с файлами
+ */
 struct Params {
-    string inFileName;
-    string inFileResult;
-    string inFileData;
-    int Port;
-    string Address;
+    string inFileName;   ///< Имя входного файла с векторами данных
+    string inFileResult; ///< Имя файла для записи результатов
+    string inFileData;   ///< Имя файла с данными аутентификации (логин и пароль)
+    int Port;           ///< Порт сервера для подключения
+    string Address;     ///< IP-адрес сервера
 };
 
-// Интерфейс
-class Interface {
+/**
+ * @class UserInterface
+ * @brief Класс для обработки параметров командной строки
+ * @details Обеспечивает парсинг, валидацию и хранение параметров командной строки
+ */
+class UserInterface {
 private:
-    // пустой парсер
-    po::options_description desc;
-    // переменная для результатов парсинга
-    po::variables_map vm;
-    // результаты парсинга параметров
-    Params params;
+    po::options_description desc; ///< Описание поддерживаемых опций
+    po::variables_map vm;         ///< Переменные для хранения распарсенных значений
+    Params params;               ///< Структура с параметрами
+    
 public:
-    Interface();
+    /**
+     * @brief Конструктор класса UserInterface
+     * @details Инициализирует опции командной строки с обязательными параметрами
+     */
+    UserInterface();
+    
+    /**
+     * @brief Парсинг аргументов командной строки
+     * @param[in] argc Количество аргументов
+     * @param[in] argv Массив аргументов
+     * @return true если парсинг успешен, false если требуется показать справку
+     */
     bool Parser(int argc, const char** argv);
+    
+    /**
+     * @brief Получение описания параметров
+     * @return Строка с описанием поддерживаемых опций
+     */
     string getDescription();
+    
+    /**
+     * @brief Получение параметров
+     * @return Структура Params с распарсенными значениями
+     */
     Params getParams() {
-    return params;
+        return params;
     };
 };
